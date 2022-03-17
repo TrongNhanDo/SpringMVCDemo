ef3907f39ceea8d97bbe4ad04f5023d74388d59f
TrongNhanDo <dotrongnhan15102000@gmail.com>
2022-03-17 15:03:00 +0700

    first commit

---
+++ src/main/java/net/codejava/spring/controller/HomeComtroller.java
@@ -0,0 +1,145 @@
+package net.codejava.spring.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import net.codejava.spring.dao.ContactDAO;
+import net.codejava.spring.model.Contact;
+
+@Controller
+public class HomeComtroller {
+	@Autowired
+	private ContactDAO contactDAO;
+
+	
+//	  public ModelAndView listContact(ModelAndView model) {
+//	      List<Contact> listContact = contactDAO.list();
+//	      model.addObject("listContact", listContact);
+//	      model.setViewName("home");
+//	   
+//	      return model; 
+//		 
+//	  }
+//	  
+	@RequestMapping(value = {"/","/viewemp"})
+	public ModelAndView Index() {
+		ModelAndView mv = new ModelAndView("home1");
+		List<Contact> listContact = contactDAO.list();
+		mv.addObject("listContact", listContact);
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/viewemp/{pageid}")
+	public ModelAndView Index1(@PathVariable int pageid) {		
+		int total = 3;
+		if (pageid != 1) {
+			pageid = (pageid - 1) * total + 1;
+		}
+		System.out.println(pageid);
+		ModelAndView mv = new ModelAndView("home1");
+		List<Contact> listContact = contactDAO.getContactByPage(pageid, total);
+		mv.addObject("listContact", listContact);
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/newContact", method = RequestMethod.GET)
+	public ModelAndView newContact(ModelAndView model) {
+		Contact newContact = new Contact();
+		model.addObject("contact", newContact);
+		model.setViewName("ContactForm");
+		return model;
+	}
+
+	@RequestMapping(value = "/saveContact", method = RequestMethod.POST)
+	public ModelAndView saveContact(@ModelAttribute Contact contact) {
+		contactDAO.saveOrUpdate(contact);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/deleteContact", method = RequestMethod.GET)
+	public ModelAndView deleteContact(HttpServletRequest request) {
+		int contactId = Integer.parseInt(request.getParameter("id"));
+		contactDAO.delete(contactId);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/editContact", method = RequestMethod.GET)
+	public ModelAndView editContact(HttpServletRequest request) {
+		int contactId = Integer.parseInt(request.getParameter("id"));
+		Contact contact = contactDAO.get(contactId);
+		ModelAndView model = new ModelAndView("ContactForm");
+		model.addObject("contact", contact);
+
+		return model;
+	}
+
+//		@Autowired
+//		EmpDao dao;// will inject dao from XML file
+	//
+//		/*
+//		 * It displays a form to input data, here "command" is a reserved request
+//		 * attribute which is used to display object data into form
+//		 */
+//		@RequestMapping("/empform")
+//		public String showform(Model m) {
+//			m.addAttribute("command", new Emp());
+//			return "empform";
+//		}
+	//
+//		/*
+//		 * It saves object into database. The @ModelAttribute puts request data into
+//		 * model object. You need to mention RequestMethod.POST method because default
+//		 * request is GET
+//		 */
+//		@RequestMapping(value = "/save", method = RequestMethod.POST)
+//		public String save(@ModelAttribute("emp") Emp emp) {
+//			dao.save(emp);
+//			return "redirect:/viewemp";// will redirect to viewemp request mapping
+//		}
+	//
+//		/* It provides list of employees in model object */
+//		@RequestMapping("/viewemp")
+//		public String viewemp(Model m) {
+//			List<Emp> list = dao.getEmployees();
+//			m.addAttribute("list", list);
+//			return "viewemp";
+//		}
+	//
+//		/*
+//		 * It displays object data into form for the given id. The @PathVariable puts
+//		 * URL data into variable.
+//		 */
+//		@RequestMapping(value = "/editemp/{id}")
+//		public String edit(@PathVariable int id, Model m) {
+//			Emp emp = dao.getEmpById(id);
+//			m.addAttribute("command", emp);
+//			return "empeditform";
+//		}
+	//
+//		/* It updates model object. */
+//		@RequestMapping(value = "/editsave", method = RequestMethod.POST)
+//		public String editsave(@ModelAttribute("emp") Emp emp) {
+//			dao.update(emp);
+//			return "redirect:/viewemp";
+//		}
+	//
+//		/* It deletes record for the given id in URL and redirects to /viewemp */
+//		@RequestMapping(value = "/deleteemp/{id}", method = RequestMethod.GET)
+//		public String delete(@PathVariable int id) {
+//			dao.delete(id);
+//			return "redirect:/viewemp";
+//		}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home1.jsp
@@ -0,0 +1,47 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+  
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Contact Manager Home</title>
+        <style type="text/css">
+        	table{
+        		width: 80%
+        	}
+        	th,td {
+        	padding : 10px}
+        </style>
+    </head>
+    <body>
+        <div align="center">
+            <h1>Contact List</h1>
+            <h3><a href='<c:url value="/newContact"></c:url>'>New Contact</a></h3>
+            <table border="1">
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Telephone</th>
+                <th colspan='2' ></th>
+                 
+                <c:forEach var="contact" items="${listContact}" varStatus="status">
+                <tr>
+                    <td>${status.index + 1}</td>
+                    <td>${contact.name}</td>
+                    <td>${contact.email}</td>
+                    <td>${contact.address}</td>
+                    <td>${contact.telephone}</td>
+                    <td><a href='<c:url value="/editContact?id=${contact.id}"></c:url>'>Edit</a></td>
+                    <td>
+                        <a href='<c:url value="/deleteContact?id=${contact.id}"></c:url>'>Delete</a>
+                    </td>
+                             
+                </tr>
+                </c:forEach>             
+            </table>
+        </div>
+    </body>
+</html>
